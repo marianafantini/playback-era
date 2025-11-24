2acae3973bbf232578c56b1cc164d1fd8137d45d
Mariana Fantini <mariana.fantini@gmail.com>
2025-11-24 00:31:28 -0300

    Update src/views/LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -31,7 +31,7 @@ const startLogin = async () => {
   const codeChallenge = base64encode(hashed);
 
   const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-  const redirectUri = window.location.origin + "/play";
+  const redirectUri = window.location.origin + "/login-success";
 
   const scope = 'user-read-private user-read-email';
   const authUrl = new URL("https://accounts.spotify.com/authorize")
