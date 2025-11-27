7bea8ec4c16701c51b4a88d133548738f28e94f3
Mariana Fantini <mariana.fantini@gmail.com>
2025-11-26 21:06:29 -0300

    lint fix

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import * as Sentry from "@sentry/vue";
+import * as Sentry from '@sentry/vue'
 
 const app = createApp(App)
 
 Sentry.init({
   app,
-  dsn: "https://3bb2254c35e9814395556db07557856e@o4510434048212999.ingest.us.sentry.io/4510434049327104",
+  dsn: 'https://3bb2254c35e9814395556db07557856e@o4510434048212999.ingest.us.sentry.io/4510434049327104',
   enableLogs: true,
-
-});
+})
 
 app.use(createPinia())
 app.use(router)
