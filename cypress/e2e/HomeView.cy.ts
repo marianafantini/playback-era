983c2778e86a998c7eeca7c195c249c26219db9b
Mariana Fantini <mariana.fantini@gmail.com>
2025-11-29 09:38:52 -0300

    format cypress files

--- cypress/e2e/HomeView.cy.ts
+++ cypress/e2e/HomeView.cy.ts
@@ -1,11 +1,11 @@
-describe('Home View', () => {
+describe("Home View", () => {
   beforeEach(() => {
-    cy.viewport('samsung-note9')
-  })
+    cy.viewport("samsung-note9");
+  });
 
-  it('visits home page and should see login with spotify button', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Playback Era')
-    cy.contains('button', 'Jogar')
-  })
-})
+  it("visits home page and should see login with spotify button", () => {
+    cy.visit("/");
+    cy.contains("h1", "Playback Era");
+    cy.contains("button", "Jogar");
+  });
+});
--- cypress/e2e/NoSongsPlayView.cy.ts
+++ cypress/e2e/NoSongsPlayView.cy.ts
@@ -1,19 +1,27 @@
-describe('Play View', () => {
+describe("Play View", () => {
   beforeEach(() => {
-    cy.viewport('samsung-note9')
-  })
+    cy.viewport("samsung-note9");
+  });
 
   it('should show "no songs" message if response is empty', () => {
-    const playlistID = "1234"
-    cy.intercept('GET', `http://localhost:3000/list-playlist-songs/${playlistID}`, { items: [] })
-    cy.visit(`/play?playlist=${playlistID}`)
-    cy.contains('Não encontramos músicas para jogar com essa playlist')
-  })
+    const playlistID = "1234";
+    cy.intercept(
+      "GET",
+      `http://localhost:3000/list-playlist-songs/${playlistID}`,
+      { items: [] },
+    );
+    cy.visit(`/play?playlist=${playlistID}`);
+    cy.contains("Não encontramos músicas para jogar com essa playlist");
+  });
 
   it('should show "no songs" message if response contains only podcasts and no songs', () => {
-    const playlistID = "1234"
-    cy.intercept('GET', `http://localhost:3000/list-playlist-songs/${playlistID}`, { fixture: 'tracks-only-podcast.json' })
-    cy.visit(`/play?playlist=${playlistID}`)
-    cy.contains('Não encontramos músicas para jogar com essa playlist')
-  })
-})
+    const playlistID = "1234";
+    cy.intercept(
+      "GET",
+      `http://localhost:3000/list-playlist-songs/${playlistID}`,
+      { fixture: "tracks-only-podcast.json" },
+    );
+    cy.visit(`/play?playlist=${playlistID}`);
+    cy.contains("Não encontramos músicas para jogar com essa playlist");
+  });
+});
