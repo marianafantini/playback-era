c8d58c815f10c532c8b559e7549f58b2f5b793dd
Mariana Fantini <mariana.fantini@gmail.com>
2025-11-29 09:43:49 -0300

    add tests for multiple phone sizes

--- cypress/e2e/HomeView.cy.ts
+++ cypress/e2e/HomeView.cy.ts
@@ -1,11 +1,11 @@
 describe("Home View", () => {
-  beforeEach(() => {
-    cy.viewport("samsung-note9");
-  });
-
-  it("visits home page and should see login with spotify button", () => {
-    cy.visit("/");
-    cy.contains("h1", "Playback Era");
-    cy.contains("button", "Jogar");
+  const mobileSizes = ['iphone-6', 'samsung-note9', 'iphone-xr'];
+  mobileSizes.forEach((size) => {
+    it(`visits home page and should see play button on mobile size ${size}`, () => {
+      cy.viewport(size);
+      cy.visit("/");
+      cy.contains("h1", "Playback Era");
+      cy.contains("button", "Jogar").should("be.visible");
+    });
   });
 });
